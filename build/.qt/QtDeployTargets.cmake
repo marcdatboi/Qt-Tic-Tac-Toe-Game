3eed765b1d71bf7892779449d9e92dae3c4e889f
NotAFerret <97323795+marcdatboi@users.noreply.github.com>
2026-03-17 16:39:46 -0700

    Delete build directory

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_tictactoe_FILE C:/DevFolder/C++/projects/qt_projects/Qt-Tic-Tac-Toe-Game/build/tictactoe.exe)
-set(__QT_DEPLOY_TARGET_tictactoe_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_tictactoe_RUNTIME_DLLS C:/DevFolder/tools/Frameworks/Qt/6.10.2/mingw_64/bin/Qt6Widgets.dll;C:/DevFolder/tools/Frameworks/Qt/6.10.2/mingw_64/bin/Qt6Gui.dll;C:/DevFolder/tools/Frameworks/Qt/6.10.2/mingw_64/bin/Qt6Core.dll)
